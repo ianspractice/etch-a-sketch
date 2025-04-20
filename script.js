e3083312286ae491cbc3bc0605ddbf55d6157040
ianspractice <bodybody.ian@gmail.com>
2025-04-19 20:19:12 -0600

    Create function to add one box to container

---
+++ script.js
@@ -0,0 +1,23 @@
+//.box width/height = (contatiner.width / number of boxes desired) - 2px; the 2px accounts for the borders
+
+const container = document.querySelector("#container");
+
+const createBox = document.createElement("div");
+
+//create 1 box
+function createBoxes() {
+  createBox.setAttribute("class", "box");
+  container.appendChild(createBox);
+}
+
+createBoxes();
+
+//add boxes to container
+
+//create hover function that changes colors of boxes with mouseenter and mouseleave (or mouseover and mouseout? research this)
+
+//create prompt to ask for number of squares per side of grid (max 100)
+
+//calculate new grid based on user response
+
+//replace current grid with new grid
