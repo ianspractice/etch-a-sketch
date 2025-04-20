1b9578922eb313c8449b1fb1f4b33de9754336e8
ianspractice <bodybody.ian@gmail.com>
2025-04-20 04:40:54 -0600

    Change function to create grid; create inital grid

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 //.box width/height = (contatiner.width / number of boxes desired) - 2px; the 2px accounts for the borders
-
+//create variables
 const container = document.querySelector("#container");
 const btn = document.querySelector("button");
 const containerStyle = window.getComputedStyle(container);
 const containerWidth = parseInt(containerStyle.getPropertyValue("width"));
 const containerHeight = parseInt(containerStyle.getPropertyValue("height"));
 
-//create 1 box
+//create initial grid
+createBoxes(16);
+
+//create boxes
 function createBoxes(choice) {
-  for (let i = 0; i < choice; i++) {
+  //empty current grid
+  container.textContent = "";
+  //create new grid
+  let gridSize = choice * choice;
+  for (let i = 0; i < gridSize; i++) {
     let createBox = document.createElement("div");
     let boxWidth = containerWidth / choice - 2 + "px";
     createBox.setAttribute("class", "box");
@@ -18,20 +25,15 @@ function createBoxes(choice) {
   }
 }
 
-//create hover function that changes colors of boxes with mouseenter and mouseleave (or mouseover and mouseout? research this)
-
 //create prompt to ask for number of squares per side of grid (max 100); add boxes to container
 btn.addEventListener("click", getGridSize);
 function getGridSize() {
-  container.innerHTML = "";
   let gridChoice = prompt("Choose a number for the sides of the grid.");
-  if (isNaN(gridChoice) || gridChoice > 100) {
-    alert("Please choose a number less than 100.");
+  if (isNaN(gridChoice) || gridChoice > 100 || gridChoice < 1) {
+    alert("Please choose a number between 1 and 100.");
+    return;
   }
-
   createBoxes(gridChoice);
 }
 
-//calculate new grid based on user response
-
-//replace current grid with new grid
+//create hover function that changes colors of boxes with mouseenter and mouseleave (or mouseover and mouseout? research this)
