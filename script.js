febba14baa5aa0ce512e2711948797e2077915e2
ianspractice <bodybody.ian@gmail.com>
2025-04-19 21:51:49 -0600

    Btn click function adds boxes to container

--- script.js
+++ script.js
@@ -6,32 +6,30 @@ const containerStyle = window.getComputedStyle(container);
 const containerWidth = parseInt(containerStyle.getPropertyValue("width"));
 const containerHeight = parseInt(containerStyle.getPropertyValue("height"));
 
-const boxWidth = containerWidth / 16 - 2 + "px";
-const grid = 32 * 32;
-
 //create 1 box
-function createBoxes() {
-  for (let i = 0; i < 16; i++) {
+function createBoxes(choice) {
+  for (let i = 0; i < choice; i++) {
     let createBox = document.createElement("div");
+    let boxWidth = containerWidth / choice - 2 + "px";
     createBox.setAttribute("class", "box");
     createBox.style.width = boxWidth;
     createBox.style.height = boxWidth;
-    container.appendChild(createBox);
+    container.append(createBox);
   }
 }
 
-//add boxes to container
-createBoxes();
-
 //create hover function that changes colors of boxes with mouseenter and mouseleave (or mouseover and mouseout? research this)
 
-//create prompt to ask for number of squares per side of grid (max 100)
+//create prompt to ask for number of squares per side of grid (max 100); add boxes to container
 btn.addEventListener("click", getGridSize);
 function getGridSize() {
+  container.innerHTML = "";
   let gridChoice = prompt("Choose a number for the sides of the grid.");
   if (isNaN(gridChoice) || gridChoice > 100) {
     alert("Please choose a number less than 100.");
   }
+
+  createBoxes(gridChoice);
 }
 
 //calculate new grid based on user response
